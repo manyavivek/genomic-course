58027ce162fd8c4dd0eaee42603754abc2ec5eb1
manyavivek <mvivek@umass.edu>
2020-08-31 15:44:20 +0000

    upload

---
+++ Lab1_MV.R
@@ -0,0 +1,21 @@
+## Exercise 1 
+x <- 3
+y <- 15
+x + y 
+x - y 
+x * y 
+x / y 
+
+## Exercise 2 
+x <- 2
+y <- 3 
+x^5 > y^4 
+
+## Exercise 3 
+x <- c(22,62,148,43,129)
+a <- sum(x)
+x/a 
+
+## Exercise 4 
+n <- "A,T,C,G"
+sort(n)
